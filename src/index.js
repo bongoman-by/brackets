692915beefd4e563d5ccea9efac2c5b57b4ea0ef
bongoman-by <bongoman@tut.by>
2020-10-15 21:54:15 +0300

    feat: done Brackets  task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let condition = true, arr = [str.trim()];
+
+    while (condition) {
+        bracketsConfig.forEach((couple) => {
+            arr.push(arr[arr.length - 1].replace(`${couple[0]}${couple[1]}`, ''));
+        });
+      
+          if (arr[arr.length - 1].length === 0) {
+        return true;
+    } 
+
+        if (arr[arr.length - 1] === arr[arr.length - 1 - bracketsConfig.length]) {
+            condition = false;
+        } 
+    }
+
+    return false;
+};
